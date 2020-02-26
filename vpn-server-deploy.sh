cdd8739d301b3b5068b54f6b4fbef57510c89281
Xavier Goffin <xavier.goffin@stack-labs.com>
2020-02-26 17:51:18 +0100

    fix(bugs): see #4

--- vpn-server-deploy.sh
+++ vpn-server-deploy.sh
@@ -7,7 +7,7 @@ sleep 5
 
 #Dependancies
 sudo apt update
-sudo apt install openvpn -y
+sudo apt install openvpn openssl -y
 
 #Install EasyRSA
 cd || exit
@@ -15,6 +15,7 @@ wget -P ~/ https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.6/EasyRSA-
 tar xvf EasyRSA-unix-v3.0.6.tgz
 file=EasyRSA-v3.0.6
 
+cd $file
 
 #CA handling
 echo "This will install the CA on the SAME machine. This is a big security risk, but if you are the single user on your VPN, it should be fine. If you do not want to install the CA here, you will have to do it manually."
@@ -22,42 +23,42 @@ echo "Type \"yes\" to install the CA here."
 read -r answer
 if [[ "$answer" == "yes" ]]
 then
-	cp "$file/vars.example" "$file/vars"
-	./$file/easyrsa init-pki
+	cp vars.example vars
+	./easyrsa init-pki
 	echo "You will need to confirm data about your CA. This is not necessarily relevant, enter whatever."
 	echo "It is recommanded to put a passphrase on your CA. Would you like one? You may have to type it several times during the script."
 	read -r answer
 	if [[ "$answer" == "yes" ]]
 	then
-		./$file/easyrsa build-ca
+		./easyrsa build-ca
 	else
-		./$file/easyrsa build-ca nopass
+		./easyrsa build-ca nopass
 	fi
 	
 	#Server request
-	./$file/easyrqa gen-req server nopass
-	sudo cp $file/pki/private/server.key /etc/openvpn
-	./$file/easyrsa sign-req server server
-	sudo cp $file/pki/issued/server.crt /etc/openvpn
-	sudo cp $file/pki/ca.crt /etc/openvpn
+	./easyrsa gen-req server nopass
+	sudo cp ~/$file/pki/private/server.key /etc/openvpn
+	./easyrsa sign-req server server
+	sudo cp ~/$file/pki/issued/server.crt /etc/openvpn
+	sudo cp ~/$file/pki/ca.crt /etc/openvpn
 
 	#Diffie-Hellmann key
-	./$file/easyrsa gen-dh
+	./easyrsa gen-dh
 	openvpn --genkey --secret ta.key
-	sudo cp $file/ta.key /etc/openvpn
-	sudo cp $file/pki/dh.pem /etc/openvpn
+	sudo cp ~/$file/ta.key /etc/openvpn
+	sudo cp ~/$file/pki/dh.pem /etc/openvpn
 
 	#Client certificate
-	mkdir -p client-configs/key
-	chmod -R 700 client-configs
+	mkdir -p ~/client-configs/keys
+	chmod -R 700 ~/client-configs
 	echo "Choose a name for your client."
 	read -r clientname
-	./$file/easyrsa gen-req "$clientname" nopass
-	cp "$file/pki/private/$clientname.key" client-configs/keys/
-	./$file/easyrsa sign-req client "$clientname"
-	cp "$file/pki/issued/$clientname.crt" client-configs/keys/
-	cp "$file/ta.key" client-configs/keys/
-	sudo cp /etc/openvpn/ca.crt client-configs/keys/
+	./easyrsa gen-req "$clientname" nopass
+	cp ~/$file/pki/private/$clientname.key ~/client-configs/keys/
+	./easyrsa sign-req client "$clientname"
+	cp ~/$file/pki/issued/$clientname.crt ~/client-configs/keys/
+	cp ~/$file/ta.key ~/client-configs/keys/
+	sudo cp /etc/openvpn/ca.crt ~/client-configs/keys/
 
 	#OVPN service conf
 	sudo cp /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz /etc/openvpn/
@@ -68,11 +69,11 @@ then
 	sudo sed -i "s/;tls-auth ta.key 0/tls-auth ta.key 0/" /etc/openvpn/server.conf
 	sudo sed -i "/cipher AES-256-CBC/ a auth SHA256" /etc/openvpn/server.conf
 	sudo sed -i "s/dh dh2048.pem/dh dh.pem/" /etc/openvpn/server.conf
-	sudo sed -i "s/;user nobody/user nobody" /etc/openvpn/server.conf
-	sudo sed -i "s/;group nogroup/group nogroup" /etc/openvpn/server.conf
+	sudo sed -i "s/;user nobody/user nobody/" /etc/openvpn/server.conf
+	sudo sed -i "s/;group nogroup/group nogroup/" /etc/openvpn/server.conf
 	sudo sed -i 's/;push "redirect-gateway def1 bypass-dhcp"/push "redirect-gateway def1 bypass-dhcp"/' /etc/openvpn/server.conf
 	sudo sed -i 's/;push "dhcp-option DNS 208.67.222.222"/push "dhcp-option DNS 208.67.222.222"/' /etc/openvpn/server.conf
-	sudo sed -i 's/;push "dhcp-option DNS 208.67.220.220"/push "dhcp-option DNS 208.67.220.220"' /etc/openvpn/server.conf
+	sudo sed -i 's/;push "dhcp-option DNS 208.67.220.220"/push "dhcp-option DNS 208.67.220.220"/' /etc/openvpn/server.conf
 
 	#Port adjustment
 	echo "You will have to select a port and protocol for the VPN. Popular choices include port udp/1194 (but might require port-forwarding on your router) or tcp/443 (default value in this script). If you do not know what you are doing, go for tcp/443. Otherwise, you can select your own protocol with \"proto\"/\"port\" and the script will use this."
@@ -98,8 +99,8 @@ then
 	fi
 	
 	#Replace protocol and port
-	sudo sed -i "s/port 1194/port $port" /etc/openvpn/server.conf
-	sudo sed -i "s/proto udp/proto $proto" /etc/openvpn/server.conf
+	sudo sed -i "s/port 1194/port $port/" /etc/openvpn/server.conf
+	sudo sed -i "s/proto udp/proto $proto/" /etc/openvpn/server.conf
 
 	if [[ "$proto" != "tcp" ]]
 	then
@@ -107,7 +108,7 @@ then
 	fi
 
 	#Server network configuration
-	sudo sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1" /etc/sysctl.conf
+	sudo sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/" /etc/sysctl.conf
 	sudo sysctl -p
 
 	#Discover network interface
@@ -147,21 +148,23 @@ EOF
 	sudo ufw allow $port/$proto
 	sudo ufw allow OpenSSH
 
-	sed -i "s/;user nobody/user nobody/" client-configs/base.conf
-	sed -i "s/;group nogroup/group nogroup/" client-configs/base.conf
-	sed -i "s/ca ca.crt/#ca ca.crt/" client-configs/base.conf
-	sed -i "s/cert client.crt/#cert client.crt/" client-configs/base.conf
-	sed -i "s/key client.key/#key client.key/" client-configs/base.conf
-	sed -i "s/tls-auth ta.key 1/#tls-auth ta.key 1/" client-configs/base.conf
-	sed -i "cipher AES-256-CBC/a auth SHA256" client-configs/base.conf
+	sed -i "s/;user nobody/user nobody/" ~/client-configs/base.conf
+	sed -i "s/;group nogroup/group nogroup/" ~/client-configs/base.conf
+	sed -i "s/ca ca.crt/#ca ca.crt/" ~/client-configs/base.conf
+	sed -i "s/cert client.crt/#cert client.crt/" ~/client-configs/base.conf
+	sed -i "s/key client.key/#key client.key/" ~/client-configs/base.conf
+	sed -i "s/tls-auth ta.key 1/#tls-auth ta.key 1/" ~/client-configs/base.conf
+	sed -i "cipher AES-256-CBC/a auth SHA256" ~/client-configs/base.conf
+	
 	{
-		key-direction 1
 
-		#Uncomment if linux client with /etc/openvpn/update-resolv-conf
-		# script-security 2
-		# up /etc/openvpn/update-resolv-conf
-		# down /etc/openvpn/update-resolv-conf
-	} >> client-configs/base.conf
+		echo "key-direction 1"
+
+		echo "#Uncomment if linux client with /etc/openvpn/update-resolv-conf"
+		echo "# script-security 2"
+		echo "# up /etc/openvpn/update-resolv-conf"
+		echo "# down /etc/openvpn/update-resolv-conf"
+	} >> ~/client-configs/base.conf
 
 	cat << 'EOF' > client-configs/make_config.sh
 #!/bin/bash
